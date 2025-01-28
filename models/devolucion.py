488ff112b1dfccfa95c0b36a9ba1e0f18bb4bbe0
AlvaroRico <alvarorico@alvarorico.com>
2025-01-28 16:21:05 +0100

    Diego la chupas?

--- models/devolucion.py
+++ models/devolucion.py
@@ -1,7 +1,15 @@
 from odoo import models, fields
 
 class Devolucion(models.Model):
-    _name = 'barbershop.barber'
-    _description = 'barber'
+    _name = 'ModuloBibliotecaOdoo.devoluciones'
+    _description = 'Devoluciones'
 
-    name = fields.Char(string = "Nombre")
+    #id = fields.Char(string = "Id", required= True)
+    fecha_devolucion = fields.Date(string = "Fecha devolucion", required= True )
+    libro = fields.Many2one(comodel_name="ModuloBibliotecaOdoo.libro", string="Libro a devolver", required= True)
+    persona = fields.Many2many(comodel_name="ModuloBibliotecaOdoo.usuario", string="Usuario", required=True)
+    profesor = fields.Many2one(comodel_name="ModuloBibliotecaOdoo.usuario", string="Encargado")
+    
+    
+    
+    
