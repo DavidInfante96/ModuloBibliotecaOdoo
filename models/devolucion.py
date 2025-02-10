b6a5249378383e6f887ba0e302e797365807637b
dnabrd04 <diegomatnf2004@gmail.com>
2025-02-10 20:06:30 +0100

    Correcccion de algunas vistas y del modelo prestamo y devolucion

--- models/devolucion.py
+++ models/devolucion.py
@@ -5,8 +5,8 @@ class Devolucion(models.Model):
     _description = 'Devoluciones'
 
     fecha_devolucion = fields.Date(string = "Fecha devolucion", required= True )
-    libro = fields.Many2one(comodel_name="biblioteca.libro", string="Libro a devolver", required= True)
-    persona = fields.Many2many(comodel_name="biblioteca.usuario", string="Usuario", required=True)
+    libro = fields.Many2many(comodel_name="biblioteca.libro", string="Libro a devolver", required= True)
+    persona = fields.Many2one(comodel_name="biblioteca.usuario", string="Usuario", required=True)
     profesor = fields.Many2one(comodel_name="biblioteca.usuario", string="Encargado")
     
     
--- models/prestamo.py
+++ models/prestamo.py
@@ -8,7 +8,7 @@ class Prestamo(models.Model):
     fecha_inicio = fields.Date( string = "Fecha inicio", required = True )
     fecha_fin = fields.Date( string = "Fecha fin", required = True )
     libro = fields.Many2one( comodel_name = "biblioteca.libro", string = "Libro", required = True )
-    persona = fields.Many2one( comodel_name = "biblioteca.usuario", string = "Persona que pide el libro", required = True )
-    profesor = fields.Many2one( comodel_name = "biblioteca.usuario", string = "Persona que gestiona", required = True )
+    persona = fields.Many2one( comodel_name = "biblioteca.usuario", string = "Solicitante", required = True )
+    profesor = fields.Many2one( comodel_name = "biblioteca.usuario", string = "Supervisor", required = True )
 
     
