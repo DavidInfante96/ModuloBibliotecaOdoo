b9e7d6cdaeeaf2afea34b1f304549799da717a28
dnabrd04 <diegomatnf2004@gmail.com>
2025-02-12 20:19:06 +0100

    Insercion de la restriccion de la validación  dni en el modelo usuario

--- models/usuario.py
+++ models/usuario.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-
+import re
 
 class usuario(models.Model):
     _name = 'biblioteca.usuario'
@@ -15,4 +15,21 @@ class usuario(models.Model):
     pais = fields.Char( string = "Pais", required = True )
     es_profesor = fields.Boolean( string = "Es profesor", required = True )
 
-    
+    @api.constrains( "dni" )
+    def _comprobar_dni(self):
+        """ Validar que el DNI sea correcto según el cálculo del módulo 23. """
+        letras_dni = "TRWAGMYFPDXBNJZSQVHLCKE"
+        dni_regex = r'^\d{8}[A-Z]$'
+
+        for record in self:
+            if not record.dni:
+                continue
+
+            if not re.match(dni_regex, record.dni):
+                raise ValidationError("El DNI debe tener 8 números seguidos de una letra mayúscula.")
+
+            numero = int(record.dni[:-1])
+            letra = record.dni[-1]
+
+            if letra != letras_dni[numero % 23]:
+                raise ValidationError("El DNI no es válido, la letra no coincide.")
