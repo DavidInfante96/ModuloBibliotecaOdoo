d61a98f2609724bb05be6cc39bcb868ed6e8da50
DavidInfante96 <david.infante.17061996@gmail.com>
2025-02-13 11:10:21 +0100

    Genero con color en autor

--- models/autor.py
+++ models/autor.py
@@ -7,6 +7,14 @@ class autor(models.Model):
 
     name = fields.Char( string = "Nombre completo", required = True )
     genero = fields.Many2one( comodel_name="biblioteca.genero", string = "Genero", required = True )
- 
 
-    
+    genero_html = fields.Html(string="Género", compute="_compute_genero_html", sanitize=False)
+
+    @api.depends('genero')
+    def _compute_genero_html(self):
+        for record in self:
+            if record.genero:
+                color = record.genero.color or "#000000"  # Usa el color hexadecimal del campo, o negro por defecto
+                record.genero_html = f'<span style="background-color: {color}; color: white; padding: 5px; border-radius: 5px; margin-right: 5px;">{record.genero.name}</span>'
+            else:
+                record.genero_html = ""
